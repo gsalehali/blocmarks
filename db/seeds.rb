2e328eb0cf4a9c61e87ca60e5796cea1103a6766
Xander Miller <xander.miller@gmail.com>
2016-03-16 13:45:56 -0400

    adds name attributes to Users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@
 5.times do
   user = User.new(
     email:    Faker::Internet.email,
-    password: Faker::Lorem.characters(10)
+    password: Faker::Lorem.characters(10),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name 
   )
   user.skip_confirmation!
   user.save!
@@ -13,6 +15,8 @@ unless User.find_by(email: 'admin@example.com')
   admin = User.new(
     email:      'admin@example.com',
     password:   'helloworld',
+    first_name: 'Admin',
+    last_name: 'Example'
   )
   admin.skip_confirmation!
   admin.save!
