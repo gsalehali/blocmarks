d7e9b365594822ec8c547d2cd79b464b83255640
gsalehali <me@salehali.net>
2016-03-16 19:17:29 +0300

    created 'full_name' method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
 end
