13089e43834e6be2023dddbe647128f56ef13192
gsalehali <me@salehali.net>
2016-03-25 13:07:11 +0300

    Generated Topics controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'topics/index'
+
+  get 'topics/show'
+
+  get 'topics/new'
+
+  get 'topics/edit'
+
   devise_for :users
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
