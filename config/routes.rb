2ca25131bd20daa8786fc2f18e60122da0a7c362
gsalehali <me@salehali.net>
2016-03-25 13:15:54 +0300

    Updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'topics/index'
-
-  get 'topics/show'
-
-  get 'topics/new'
-
-  get 'topics/edit'
-
   devise_for :users
   get 'about' => 'welcome#about'
+
   root to: 'welcome#index'
+
   post :incoming, to: 'incoming#create'
+
+  resources :topics
 
 end
