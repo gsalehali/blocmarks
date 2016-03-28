83c9babdb61d8a71dc8655898747cef1b216978d
gsalehali <me@salehali.net>
2016-03-28 18:53:24 +0300

    Added will_pagination gem, And Added thumbnail and title attributs to Bookmark

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316213254) do
+ActiveRecord::Schema.define(version: 20160327145223) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "thumbnail"
+    t.string   "title"
   end
 
   add_index "bookmarks", ["topic_id"], name: "index_bookmarks_on_topic_id"
