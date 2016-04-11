943ca49707f2f0416f0671ce74a813bedd1d104d
gsalehali <me@salehali.net>
2016-04-11 11:07:45 +0300

    Created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, only: [:show]
+
   get 'about' => 'welcome#about'
-
   root to: 'welcome#index'
-
   post :incoming, to: 'incoming#create'
 
   resources :topics do
