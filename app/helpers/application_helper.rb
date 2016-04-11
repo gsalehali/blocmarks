f6bf20e60498f505b8fd55c7d67d5e0974318d3f
gsalehali <me@salehali.net>
2016-04-11 07:49:53 +0300

    Added more styel to bookmark#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,12 @@ module ApplicationHelper
     end
   end
 
+  def add_btn_sm(controller)
+    if controller.class.to_s.downcase.exclude?('bookmark')
+      return 'btn-sm'
+    end
+  end
+
   def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group row has-danger'
